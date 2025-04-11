c82a18e3b260bf5ab355c0d39bfaae3ad8358ba1
LocalPlayer463 <hd6dysyd@gmail.com>
2025-04-12 01:06:51 +0700

    Create testw.lua

---
+++ testw.lua
@@ -0,0 +1,14 @@
+local key = getgenv().Key
+local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+
+-- ฝั่งเซิร์ฟเวอร์จะตรวจสอบว่า key ถูกใช้หรือยัง แล้วถ้า hwid ยังไม่ถูกตั้ง ก็จะอัปเดตให้
+local url = "http://192.168.1.15:3000/checkandupdate?key=" .. key .. "&hwid=" .. hwid
+
+local response = game:HttpGet(url)
+
+local result = loadstring(response)
+if result then
+    result() -- รันสคริปต์ที่ฝั่งเซิร์ฟเวอร์ส่งกลับมา
+else
+    warn("Invalid script from server")
+end
