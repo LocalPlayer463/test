ab8d261dc3ae35a9949b0afcf6241bde217fc6ce
LocalPlayer463 <hd6dysyd@gmail.com>
2025-04-12 01:24:14 +0700

    Update testw.lua

--- testw.lua
+++ testw.lua
@@ -1,15 +1,19 @@
-local key = getgenv().Key
+local HttpService = game:GetService("HttpService")
 local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+local key = getgenv().Key
 
--- ฝั่งเซิร์ฟเวอร์จะตรวจสอบว่า key ถูกใช้หรือยัง แล้วถ้า hwid ยังไม่ถูกตั้ง ก็จะอัปเดตให้
-local url = "http://192.168.1.15:3000/checkandupdate?key=" .. key .. "&hwid=" .. hwid
+local data = HttpService:JSONEncode({
+    key = key,
+    hwid = hwid
+})
 
-local response = game:HttpGet(url)
+local response = syn.request({
+    Url = "http://localhost:3000/checkandupdate", -- เปลี่ยนเป็น URL ที่คุณโฮสต์ไว้
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = data
+})
 
-local result = loadstring(response)
-if result then
-    result() -- รันสคริปต์ที่ฝั่งเซิร์ฟเวอร์ส่งกลับมา
-    print "Yes"
-else
-    warn("Invalid script from server")
-end
+loadstring(response.Body)()
